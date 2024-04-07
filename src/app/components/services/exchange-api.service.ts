69a0d61b41d68d951dd5f9245617a9d4aa6692d3
trytocatcharg <blockchain@FSN>
2024-04-08 00:02:54 +0200

    minor fixes

--- src/app/components/services/exchange-api.service.ts
+++ src/app/components/services/exchange-api.service.ts
@@ -12,29 +12,29 @@ export class ExchangeApiService {
   private api_key = 'DsHJFEaj5RbyT65CF7UGmYVl6HoTN1QC';
 
   public getExchangeRates(base: string) {
-    // return this.httpClient.get('assets/'+base+'.json').pipe(
-    //   delay(2000),
-    //        map((res: any) => {
-    //     const rates = Object.entries(res.rates);
-    //    return {
-    //      base: res.base,
-    //      date: res.date,
-    //      rates: rates.map(([currency, value]) => {
-    //        return { currency, value } as Rate;
-    //      })
-    //    } as unknown as ExchangeModels
-    //   }));
-    return  this.httpClient.get(this.ENDPOINT_API + base, { headers: { apikey: this.api_key } }).pipe(
-      map((res: any) => {
+    return this.httpClient.get('assets/'+base+'.json').pipe(
+      delay(2000),
+           map((res: any) => {
         const rates = Object.entries(res.rates);
        return {
-          base: res.base,
-          date: res.date,
-          rates: rates.map(([currency, value]) => {
-            return { currency, value } as Rate;
-          })
-        } as ExchangeModels
+         base: res.base,
+         date: res.date,
+         rates: rates.map(([currency, value]) => {
+           return { currency, value } as Rate;
+         })
+       } as unknown as ExchangeModels
       }));
+    // return  this.httpClient.get(this.ENDPOINT_API + base, { headers: { apikey: this.api_key } }).pipe(
+    //   map((res: any) => {
+    //     const rates = Object.entries(res.rates);
+    //    return {
+    //       base: res.base,
+    //       date: res.date,
+    //       rates: rates.map(([currency, value]) => {
+    //         return { currency, value } as Rate;
+    //       })
+    //     } as ExchangeModels
+    //   }));
   }
 
 }
